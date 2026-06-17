762f581d868ed3a4d64e5b443870d42393734643
eis <eis@iki.fi>
2026-06-17 14:08:28 +0300

    Added test case for java.util.logging

---
+++ src/test/java/fi/eis/libraries/di/logger/SimpleJULLoggerTest.java
@@ -0,0 +1,115 @@
+package fi.eis.libraries.di.logger;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Filter;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import static fi.eis.libraries.di.testhelpers.LoggingConfigHelper.resetJULLoggingConfiguration;
+import static fi.eis.libraries.di.testhelpers.LoggingConfigHelper.setJULLoggingConfiguration;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.not;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class SimpleJULLoggerTest {
+
+    private final List<String> logs = new ArrayList<>();
+    private final Filter recordingFilter = record -> {
+        logs.add(record.getLevel() + " " + record.getMessage());
+        return true;
+    };
+    private Filter previousFilter;
+
+g    private final Logger julLogger = Logger.getLogger(this.getClass().getSimpleName());
+
+    @Before
+    public void loggingSetup() {
+        previousFilter = julLogger.getFilter();
+        julLogger.setFilter(recordingFilter);
+
+        setJULLoggingConfiguration();
+    }
+
+    @After
+    public void loggingReset() {
+        julLogger.setFilter(previousFilter);
+        logs.clear();
+        
+        resetJULLoggingConfiguration();
+    }
+
+    @Test
+    public void testDebugLevelDebugLogging() {
+        // Given
+        SimpleLogger logger = new SimpleLogger(this.getClass());
+        logger.setLogLevel(LogLevel.DEBUG);
+        assertTrue(logger.isDebugEnabled());
+        assertTrue(logger.isErrorEnabled());
+
+        // When
+        logger.debug("Hello");
+        logger.debug("How're you doing, %s?", "John");
+
+        // Then
+        String result = logs.stream().collect(Collectors.joining());
+        
+        assertThat(result, containsString("Hello"));
+        assertThat(result, containsString("How're you doing, John?"));
+    }
+
+    @Test
+    public void testErrorLevelDebugLogging() {
+        SimpleLogger logger = new SimpleLogger(this.getClass());
+        logger.setLogLevel(LogLevel.ERROR);
+        assertFalse(logger.isDebugEnabled());
+        assertTrue(logger.isErrorEnabled());
+
+        logger.debug("Hello");
+        logger.debug("How're you doing, %s?", "John");
+
+        String result = logs.stream().collect(Collectors.joining());
+        assertThat(result, not(containsString("DEBUG")));
+        assertThat(result, not(containsString("How're you doing, John?")));
+    }
+
+    @Test
+    public void testNoneLevelDebugLogging() {
+        SimpleLogger logger = new SimpleLogger(this.getClass());
+        logger.setLogLevel(LogLevel.NONE);
+        assertFalse(logger.isDebugEnabled());
+        assertFalse(logger.isErrorEnabled());
+
+        logger.debug("Hello");
+        logger.debug("How're you doing, %s?", "John");
+        logger.error("Some error");
+
+        String result = logs.stream().collect(Collectors.joining());
+        assertThat(result, not(containsString("DEBUG")));
+        assertThat(result, not(containsString("How're you doing, John?")));
+        assertThat(result, not(containsString("ERROR")));
+    }
+
+    @Test
+    public void testErrorLevelErrorLogging() {
+        SimpleLogger logger = new SimpleLogger(this.getClass());
+        logger.setLogLevel(LogLevel.ERROR);
+        assertFalse(logger.isDebugEnabled());
+        assertTrue(logger.isErrorEnabled());
+
+        logger.debug("Hello");
+        logger.debug("How're you doing, %s?", "John");
+        logger.error("Quite well, %s.", "Matt");
+
+        String result = logs.stream().collect(Collectors.joining());
+        assertThat(result, not(containsString("DEBUG")));
+        assertThat(result, not(containsString("How're you doing, John?")));
+        assertThat(result, containsString("Quite well, Matt."));
+    }
+}
--- src/test/java/fi/eis/libraries/di/testhelpers/LoggingConfigHelper.java
+++ src/test/java/fi/eis/libraries/di/testhelpers/LoggingConfigHelper.java
@@ -1,10 +1,34 @@
 package fi.eis.libraries.di.testhelpers;
 
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.LogManager;
+
 import static fi.eis.libraries.di.logger.SimpleLogger.PROPERTY_NAME_USE_SYSTEM_OUT_LOGGING;
 
 public class LoggingConfigHelper {
     private static String systemOutPropertyValueHolder;
+    private static String julConfigFilePropertyValueHolder;
 
+    private static final String JUL_CONFIG_FILE_PROPERTY_NAME = "java.util.logging.config.file";
+
+    public static void setJULLoggingConfiguration() {
+        julConfigFilePropertyValueHolder = System.getProperty(JUL_CONFIG_FILE_PROPERTY_NAME);
+
+        // Figure out the path to my test file
+        String resourceName = "my-test-logging.properties";
+        ClassLoader classLoader = LoggingConfigHelper.class.getClassLoader();
+        File file = new File(classLoader.getResource(resourceName).getFile());
+        String absolutePath = file.getAbsolutePath();
+
+        // use that path
+        System.setProperty(JUL_CONFIG_FILE_PROPERTY_NAME, absolutePath);
+        try {
+            LogManager.getLogManager().readConfiguration();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     public static void setSystemOutLogging() {
         systemOutPropertyValueHolder = System.getProperty(PROPERTY_NAME_USE_SYSTEM_OUT_LOGGING);
         System.setProperty(PROPERTY_NAME_USE_SYSTEM_OUT_LOGGING, "true");
@@ -15,6 +39,18 @@ public class LoggingConfigHelper {
             System.clearProperty(PROPERTY_NAME_USE_SYSTEM_OUT_LOGGING);
         } else {
             System.setProperty(PROPERTY_NAME_USE_SYSTEM_OUT_LOGGING, systemOutPropertyValueHolder);
+        }
+    }
+    public static void resetJULLoggingConfiguration() {
+        if (julConfigFilePropertyValueHolder == null) {
+            System.clearProperty(JUL_CONFIG_FILE_PROPERTY_NAME);
+        } else {
+            System.setProperty(JUL_CONFIG_FILE_PROPERTY_NAME, julConfigFilePropertyValueHolder);
+        }
+        try {
+            LogManager.getLogManager().readConfiguration();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
